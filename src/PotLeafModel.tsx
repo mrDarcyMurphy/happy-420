255e0eb10321d2358ccb2f50cf337101a403595e
Mr. Darcy Murphy <darcy.murphy@loudbit.com>
2024-04-20 15:05:36 -0500

    fix typescript issues

--- src/PotLeafModel.tsx
+++ src/PotLeafModel.tsx
@@ -1,27 +1,28 @@
 import { useGLTF } from '@react-three/drei'
+import { Mesh } from 'three'
 
-export function PotLeafModel(props) {
+export function PotLeafModel(props: any) {
   const { nodes, materials } = useGLTF('/pot-leaf.glb')
   return (
     <group {...props} dispose={null}>
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Cannabis_Leaf_Realistic.geometry}
+        geometry={(nodes.Cannabis_Leaf_Realistic as Mesh).geometry}
         material={materials.Cannabis_Leaf_Realistic}
         scale={0.082}
       />
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Ring.geometry}
+        geometry={(nodes.Ring as Mesh).geometry}
         material={materials.Ring}
         scale={0.082}
       />
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Disc.geometry}
+        geometry={(nodes.Disc as Mesh).geometry}
         material={materials.Material}
         scale={0.082}
       />
