a8bea43c74297e2ce1b62c9195dd3c3ae1ddcfd1
Mr. Darcy Murphy <darcy.murphy@loudbit.com>
2024-04-20 15:33:18 -0500

    add link to AR model

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import { PotLeafScene } from './PotLeafScene'
 
 function App() {
   return (
-    <Canvas id="canvas" shadows>
-      <PotLeafScene />
-    </Canvas>
+    <>
+      <Canvas id="canvas" shadows>
+        <PotLeafScene />
+      </Canvas>
+      <div id="overlay">
+        <a href="/happy-420/pot-leaf.usdz">View it in AR</a>
+      </div>
+    </>
   )
 }
 
